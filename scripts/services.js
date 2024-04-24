95bbeb9144f19cf57c765fda1327d3a5ada2c276
DaishaMcC <daisha05311994@gmail.com>
2024-04-24 01:14:35 -0400

    PetSalon HW

--- scripts/services.js
+++ scripts/services.js
@@ -2,8 +2,19 @@ let services=[];
 
 //
 
-//
+// costructor
+function Pet(service,action){
+    this.service=service;
+    this.action=action;
+}
 
-//
+// register function
+function register(){
+    let inputService = document.getElementById("txtService").value;
+    let inputAction = document.getElementById("txtAction").value;
+}
 
-//
+// use notification
+function showNotification(){
+    $("#Notification").show();
+}
