0d9798d6caeca4a1734f07ad911b6d9309e8d881
DaishaMcC <daisha05311994@gmail.com>
2024-04-24 11:33:38 -0400

    petsalon

--- scripts/services.js
+++ scripts/services.js
@@ -8,6 +8,19 @@ function Pet(service,action){
     this.action=action;
 }
 
+function isValid(pet){
+    let validation=true; // we are fine
+    if(pet.service==""){    
+    validation=false
+    alert("Please add service","error");
+    }
+    if(pet.action==""){
+        validation=false
+        alert("Please add...","error");
+    }
+    return validation;  //returning the result of the validation
+}
+
 // register function
 function register(){
     let inputService = document.getElementById("txtService").value;
